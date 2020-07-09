72e3e254e77ce2a868380264b2ee51ba19da4d96
Guilherme <guirlviana@gmail.com>
2020-07-09 13:46:02 -0300

    prints e leitura de variaveis

---
+++ game.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int num = 42;
+    printf("Jogo da adivinhacao\n");
+    
+    int chute;
+    printf("Qual e o seu chute? ");
+    scanf("%d", &chute);
+    printf("O numero: %d \n", num);
+    printf("Seu chute foi %d \n", chute);
+}
